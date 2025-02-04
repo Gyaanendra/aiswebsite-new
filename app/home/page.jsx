48f1fcb2b2409214d17a2c1421b52e7ef1e8db85
Afjal Hussein <insaneafjal@gmail.com>
2025-02-04 22:15:20 +0530

    allchanges

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -2,13 +2,14 @@ import LandingBanner from "@/components/home/LandingAnimation";
 // Import the new component
 import Aboutus from "@/screens/aboutus";
 import SplashCursor from "@/Animations/SplashCursor/SplashCursor";
+import ParticlesComponent from "@/components/Particles";
 export default function Home() {
   return (
     <main className="bg-black">
       {/* Add the particle background */}
-      <section className="h-auto">
-        <LandingBanner />
-      </section>
+      <LandingBanner />
+      <ParticlesComponent id="particles-1" />
+
       {/* <section className="mt-10">
         <Aboutus />
       </section> */}
